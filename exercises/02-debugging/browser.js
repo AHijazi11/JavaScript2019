91415de8ad9b3a30d81ac5c5accb981c62e6d8a3
AHijazi11 <ahmadhijazi0@gmail.com>
2019-02-24 14:21:18 -0500

    command-line exercise

--- exercises/02-debugging/browser.js
+++ exercises/02-debugging/browser.js
@@ -34,20 +34,20 @@ function getMessage() {
   var message = "";
 
   for (var i = 1; i < 10; i++) {
-    mesage = message + "...";
+    message = message + "...";
   }
 
   message = message + "\n"; // New line
 
   var fruits = ["apple", "kiwi", "banana", "orange", "blueberry"];
   for (var j = 0; j < fruits.length; j++) {
-    message = message + String(i) + " " + fruits[j] + "\n";
+    message = message + String((j+1)) + " " + fruits[j] + "\n";
   }
 
   var moreFruits = ["cranberry", "mango", "passion fruit", "avocado", "cherry"];
   for (var k = 0; k < moreFruits.length; k++) {
-    var num = k + fruits.length;
-    message = message + String(num) + " " + fruits[k] + "\n";
+    var num = k + 1 + fruits.length;
+    message = message + String(num) + " " + moreFruits[k] + "\n";
   }
 
   for (var z = 1; z < 10; z++) {
