a50d83d0e4f709387611aa63f1f857ba86b8db4c
AHijazi11 <ahmadhijazi0@gmail.com>
2019-03-13 23:08:26 -0400

    React Address.jsx challenge

--- react-exercises/12-address-form/src/components/Address/Address.jsx
+++ react-exercises/12-address-form/src/components/Address/Address.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./Address.css";
-// Import data from "assets/countries.js" and "assets/states.js" here
+import countries from "../../assets/countries";
+import states from '../../assets/states'
 
 class Address extends Component {
   state = {
@@ -10,12 +11,24 @@ class Address extends Component {
     city: "",
     state: "",
     postalCode: "",
-    ZipCode: ""
+    Country: "",
+    Submitted : false
   };
 
+  setfirstName = e => {this.setState({firstName : e.target.value})}
+  setlastName = e => {this.setState({lastName : e.target.value})}
+  setaddressLine1 = e => {this.setState({addressLine1 : e.target.value})}
+  setcity = e => {this.setState({city : e.target.value})}
+  setstate = e => {this.setState({state : e.target.value})} 
+  setpostalcode = e => {this.setState({postalCode : e.target.value})}
+  setCountry = e => {this.setState({Country : e.target.value})}
+  handleSubmit = e => {e.preventDefault(); this.setState({Submitted : true})}
+
+
   render() {
+    console.log(countries)
     return (
-      <form className="container mt-4">
+      <form className="container mt-4" onSubmit = {this.handleSubmit}>
         <div className="form-group">
           <label htmlFor="firstName" className="control-label">
             First Name
@@ -25,6 +38,8 @@ class Address extends Component {
             name="firstName"
             type="text"
             className="form-control"
+            value = {this.state.firstName}
+            onChange = {this.setfirstName}
           />
         </div>
         <div className="form-group">
@@ -36,6 +51,8 @@ class Address extends Component {
             name="lastName"
             type="text"
             className="form-control"
+            value = {this.state.lastName}
+            onChange = {this.setlastName}
           />
         </div>
         <div className="form-group">
@@ -47,6 +64,8 @@ class Address extends Component {
             name="addressLine1"
             type="text"
             className="form-control"
+            value = {this.state.addressLine1}
+            onChange = {this.setaddressLine1}
           />
           <p className="help-block text-muted">
             Street address, P.O. box, company name, c/o
@@ -57,14 +76,26 @@ class Address extends Component {
           <label htmlFor="city" className="control-label">
             City / Town
           </label>
-          <input id="city" name="city" type="text" className="form-control" />
+          <input id="city" name="city" type="text" className="form-control" 
+          value = {this.state.city}
+          onChange = {this.setcity}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="state" className="control-label">
             State / Province / Region
           </label>
-          {/* Loop through the states you imported here */}
-          <select id="state" name="state" className="form-control" />
+          <select id="state" name="state" className="form-control"  
+          value = {this.state.state} 
+          onChange = {this.setstate}>
+          {
+            states.map((state,idx) => {
+              return (
+              <option>{state}</option>
+              )
+              })
+              }
+          </select>
         </div>
 
         <div className="form-group">
@@ -76,6 +107,8 @@ class Address extends Component {
             name="postalCode"
             type="text"
             className="form-control"
+            value = {this.state.postalCode}
+            onChange = {this.setpostalcode}
           />
         </div>
 
@@ -83,8 +116,17 @@ class Address extends Component {
           <label htmlFor="country" className="control-label">
             Country
           </label>
-          {/* Loop through the countries you imported here */}
-          <select id="country" name="country" className="form-control" />
+          <select id="country" name="country" className="form-control"
+          value = {this.state.country}
+          onChange = {this.setCountry} >
+          {
+            countries.map((country,idx) => {
+              return (
+                <option>{country}</option>
+              )
+            })
+          }
+          </select>
         </div>
         <button type="submit" className="btn btn-primary">
           Submit
@@ -94,11 +136,17 @@ class Address extends Component {
          * Find a way to only display this once the form has been submitted.
          * Hint: You will need to change this "this.condtion" below to something else
          */}
-        {this.condition && (
+        {this.state.Submitted && (
           <div className="card card-body bg-light mt-4 mb-4">
             Results:
             <ul className="list-unstyled mb-0">
-              {/* Add <li></li> tags here */}
+              <li>{this.state.firstName}</li>
+              <li>{this.state.lastName}</li>
+              <li>{this.state.addressLine1}</li>
+              <li>{this.state.city}</li>
+              <li>{this.state.state}</li>
+              <li>{this.state.postalCode}</li>
+              <li>{this.state.Country}</li>
             </ul>
           </div>
         )}
